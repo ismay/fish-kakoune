8de1a384786bff54dbc3415a1b758f6ebd025cdd
ismay <7355199+ismay@users.noreply.github.com>
2022-04-22 10:28:25 +0200

    Split kakoune session handling

--- functions/k.fish
+++ functions/k.fish
@@ -1,28 +1,12 @@
 function k --description "Connect to existing kakoune session or create a new one" --argument-names filename
-    if test -n "$filename"
-        # Use the working dir of the filename if one was passed
-        set -f file_basedir (dirname (realpath $filename))
-    else
-        # Use the current working directory if there's no filename
-        set -f file_basedir (pwd)
-    end
+    set -l session (kakoune_session $filename)
+    set -l match (kak -l | grep $session)
 
-    set -l git_toplevel (fish -c "cd $file_basedir && git rev-parse --show-toplevel 2>/dev/null")
-
-    if test -z $git_toplevel
-        # Run a disconnected session if not in a git repo
-        kak $filename
-        return
-    end
-
-    set -l projectname (basename $git_toplevel | string replace . -)
-    set -l session (kak -l | grep $projectname)
-
-    if test -z "$session"
-        # Create a new session if there are none
-        kak -s $projectname $filename
+    if test -z "$match"
+        # Create a new session if there is no match
+        kak -s $session $filename
     else
         # Otherwise connect to existing session
-        kak -c $projectname $filename
+        kak -c $session $filename
     end
 end
---
+++ functions/kakoune_session.fish
@@ -0,0 +1,20 @@
+function kakoune_session --description "Get session name from filename" --argument-names filename
+    if test -n "$filename"
+        # Use the working dir of the filename if one was passed
+        set -f file_basedir (dirname (realpath $filename))
+    else
+        # Use the current working directory if there's no filename
+        set -f file_basedir (pwd)
+    end
+
+    set -l git_toplevel (fish -c "cd $file_basedir && git rev-parse --show-toplevel 2>/dev/null")
+
+    if test -z $git_toplevel
+        # Echo the basename of the folder the target file resides in if not in git repo
+        echo (basename $file_basedir)
+        return
+    end
+
+    # Echo the name of the folder at the git root
+    echo (basename $git_toplevel | string replace . -)
+end
