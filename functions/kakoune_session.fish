a99582e1f359a0a25523923e3e5ac92c7fac7130
ismay <7355199+ismay@users.noreply.github.com>
2022-05-27 09:27:23 +0200

    Escape slash

--- functions/kakoune_session.fish
+++ functions/kakoune_session.fish
@@ -11,10 +11,10 @@ function kakoune_session --description "Get session name from filename" --argume
 
     if test -z $git_toplevel
         # Echo the basename of the target folder if not in git repo
-        echo (basename $file_basedir | string replace . -)
+        echo (basename $file_basedir | string replace . - | string replace / root)
         return
     end
 
     # Echo the name of the folder at the git root
-    echo (basename $git_toplevel | string replace . -)
+    echo (basename $git_toplevel | string replace . - | string replace / root)
 end
